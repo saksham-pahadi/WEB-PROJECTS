584de688c454fa781bbd316a879e1ccb913c4053
saksham-pahadi <saksham-pahadi@users.noreply.github.com>
2025-08-27 12:43:05 +0530

    fix some issue

--- bitlinks/app/shorten/page.js
+++ bitlinks/app/shorten/page.js
@@ -9,6 +9,7 @@ const Shorten = () => {
     const [generated, setgenerated] = useState(false)
     const [copy, setcopy] = useState(false)
     const [Result, setresult] = useState("")
+    const [loading, setloading] = useState(false)
 
     const copyurl = () => {
         navigator.clipboard.writeText(generated)
@@ -33,9 +34,9 @@ const Shorten = () => {
         fetch("/api/generate", requestOptions)
             .then((response) => response.json())
             .then((result) => {
-                console.log(result.success)
                 if (result.success) {
                     setgenerated(`${process.env.NEXT_PUBLIC_HOST}/${shorturl}`)
+                    setloading(false)
                     seturl("")
                     setshorturl("")
                 }
@@ -55,11 +56,11 @@ const Shorten = () => {
             <div className=' flex flex-col gap-2 py-5 w-1/2 mx-auto items-center'>
                 <input className='border-2 rounded-lg w-full md:w-1/2 p-2 border-purple-500' type="text" placeholder='Enter your URL' value={url} onChange={e => { seturl(e.target.value) }} />
                 <input className='border-2 rounded-lg w-full md:w-1/2 p-2  border-purple-500' type="text" placeholder='Enter your preferred short URL text' value={shorturl} onChange={e => { setshorturl(e.target.value) }} />
-                <button onClick={() => { generate(),setcopy(false) }} className='bg-purple-800 p-2 rounded-lg text-white'>Generate</button>
+                <button onClick={() => { generate(),setcopy(false),setloading(!loading) }} className='bg-purple-800 p-2 rounded-lg text-white cursor-pointer'>{loading?"Loading...":"Generate"}</button>
 
                 {generated && <> <span className='font-bold text-lg'>Your Link </span><code><Link target="_blank" href={generated}>{generated}</Link>
                 </code>
-                    <button className={`${copy?"bg-green-600":"bg-purple-800"} text-white p-2 rounded-lg`} onClick={() => { copyurl(), setcopy(!copy) }}>{copy ? "Copied" : "Copy"}</button>
+                    <button className={`${copy?"bg-green-600":"bg-purple-800"} text-white p-2 cursor-pointer rounded-lg`} onClick={() => { copyurl(), setcopy(!copy) }}>{copy ? "Copied" : "Copy"}</button>
                 </>}
                 {!generated && <> <p className='text-center text-red-500 font-semibold'>{Result}</p> 
                 </>}
