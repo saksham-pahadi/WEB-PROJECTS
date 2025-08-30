7ee57a4020274fb48c5892161bc1a52eb1234fdc
saksham-pahadi <saksham-pahadi@users.noreply.github.com>
2025-08-30 12:38:22 +0530

    feat: initialize highway-delite project with Next.js, React, and Tailwind CSS.
    Created Navbar & Footer components with signup and signin button .

---
+++ highway-delite/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div className="footer bg-gray-200 text-center p-4">
+            <p className='text-gray-600'>&copy; 2024 Highway Delite. All rights reserved.</p>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
