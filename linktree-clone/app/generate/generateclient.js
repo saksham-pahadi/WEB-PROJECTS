10a7d15592121f121f2c606a93ed8e9e39445aa2
saksham-pahadi <saksham-pahadi@users.noreply.github.com>
2025-09-11 22:05:16 +0530

    Update url

--- linktree-clone/app/generate/generateclient.js
+++ linktree-clone/app/generate/generateclient.js
@@ -89,7 +89,7 @@ const GenerateClient = () => {
             redirect: "follow"
         };
 
-        const r = await fetch("http://localhost:3000/api/add", requestOptions)
+        const r = await fetch(`${NEXT_PUBLIC_HOST}/api/add`, requestOptions)
         const result = await r.json()
         if (!result.success) {
             toast.error(result.message)
